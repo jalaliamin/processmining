318e018d0bc5b06ce515ce8ebaafe6de2633f994
Amin <jalali.amin@gmail.com>
2025-03-05 14:34:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setuptools.setup(
     name="#{Build.DefinitionName}#",
     version="#{version}#", 
     description="Python #{Build.DefinitionName}# Package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jalaliamin/processmining',
+    author='Your Name',
+    author_email='your_email@example.com',
+    license='MIT',
     packages=setuptools.find_packages('src'),
     package_dir={'':'src'},
     install_requires=[
